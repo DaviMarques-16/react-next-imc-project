4db7de01ba353070061bbe1bf9e31a5f7cbb32d1
Davi Marques <142530449+DaviMarques-16@users.noreply.github.com>
2025-09-27 23:30:09 -0300

    chore: funções e tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,45 @@
 const nome = 'Davi'
 let canal ='Dazuzin'
 
-function retnome(){
+function retnome(){ //função padrão
   return nome
 }
+
+const arrowfunction = ()=> {
+  return canal
+}
+
+const show = (artista: string, local: string)=> {
+  return `O Show será Taycabana: ${artista} em ${local}`
+}
+
+function Topo(){
+  return(
+    <div className="flex justify-evenly items-center bg-zinc-300 
+    h-[100px]">
+      <div>i can do it</div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-4xl">{canal}</div>
+        <div className="subtituloTopo">Hi, i have a question</div>
+        <div>{nome}</div>
+      </div>
+      <div>Cruel Summer</div>
+    </div>
+  )
+}
+
 
 //contexto local à função (escopo)
 export default function Home() {
   //Cada componente importa somente uma tag (ex: a div)
   return (
     <div>
+        <Topo/> {/* componente */}
         <div>Evermore</div>
         <div>Midnights</div>
         <div>THE TORTURED POETS DEPARTMENT</div>
         <div>The Life Of a Showgirl</div>
-        <div>{nome}, {canal}, {retnome()}</div>
+        <div>{show("Taylor Swift", "Copacabana")}</div>
     </div>
   );
 }
